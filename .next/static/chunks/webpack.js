c5db9dc8d445bb31acb6dc564c4f047df0ee2b50
berkagmpp <berkagmpp@gmail.com>
2022-06-26 16:14:46 +1200

    fix: Fix Link(next) in MainNavigation.js Revise from 'Link(react-router-dom) to' to 'Link(next/link) href' in MainNavigation.js for Nextjs syntex

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c533daf3591b6fa"; }
+/******/ 		__webpack_require__.h = function() { return "be704e416453ec36"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
