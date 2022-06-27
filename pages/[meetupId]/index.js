6e31faed3ce28080c8d88df35b5ce9cfc96cc403
berkagmpp <berkagmpp@gmail.com>
2022-06-27 13:55:53 +1200

    feat: Add MeetupDetail component which is passed props from [meetupId]>index.js Add MeetupDetail component for setting CSS styling Set JSX codes to be passed props Revise [meetupId]>index.js to pass data to the MeetupDetail component Ref: #2a89fe951ee2b3ae55b09f2694f8f825ec399bb7

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,11 @@
-import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetails = () => {
     return (
-        <Fragment>
-            <img src="https://www.interest.co.nz/sites/default/files/feature_images/65%20Federal%20Harbour%20View.jpg" 
-                 alt="A Fisrt Meetup" />
-            <h1>The First Meetup</h1>
-            <address>5 Howe Street, Freemansbay, Auckland</address>
-            <p>This is the first meetup!</p>
-        </Fragment>
+        <MeetupDetail image="https://www.interest.co.nz/sites/default/files/feature_images/65%20Federal%20Harbour%20View.jpg"
+                      title="The First Meetup"
+                      address="5 Howe Street, Freemansbay, Auckland"
+                      description="This is the first meetup!" />
     );
 };
 
