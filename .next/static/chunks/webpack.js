d2ccf0b7c70545ff7009644eaad98574fd6f2a52
berkagmpp <berkagmpp@gmail.com>
2022-06-28 19:52:35 +1200

    feat: Add pages>api>new-meetup file for attaching api of new-meetup.js Add pages>api>new-meetup file Add handler hn with req, res as parameters Define POST condition of data

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5fed0b34b65cbb25"; }
+/******/ 		__webpack_require__.h = function() { return "7522031fedcb7a5d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
