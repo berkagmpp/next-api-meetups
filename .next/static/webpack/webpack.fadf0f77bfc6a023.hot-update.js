c577b7ad3adcce7f8bc06da6c238056cfa38d54d
berkagmpp <berkagmpp@gmail.com>
2022-06-29 11:50:20 +1200

    feat: Add MongoDB connect in handler fn of pages>api>new-meetup Add const client to connect with MongoDB Add async and await in the handler fn Set const db and it's collection Add const result with await using insertOne() to insert 1 new documents (data) into colloction Delete the destructured data Ref: #d2ccf0b7c70545ff7009644eaad98574fd6f2a52

--- .next/static/webpack/webpack.fadf0f77bfc6a023.hot-update.js
+++ .next/static/webpack/webpack.fadf0f77bfc6a023.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fc7a45e37691c64c"; }
+/******/ 	__webpack_require__.h = function() { return "35611f51b221274d"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
