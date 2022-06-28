2d9516376ac85471c79a241e8b252ee9eb337a4a
berkagmpp <berkagmpp@gmail.com>
2022-06-28 14:00:58 +1200

    fix: Add getStaticPaths fn in [meetupId]>index.js Add getStaticPaths fn which returns paths: [ ], fallback: boolean Resolve: #ef42ebefa5269a89acbc066dcee9d0abe574b55c

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -9,6 +9,27 @@ const MeetupDetails = () => {
     );
 };
 
+export async function getStaticPaths() {
+
+    return {
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                },
+
+            }
+        ],
+        fallback: false // when the user enters /m3 -> false: shows 404 error, true: NextJs would try to generate a page for m3 id dynamically on the server
+    };
+};
+
 export async function getStaticProps(context) {
     const meetupId = context.params.meetupId;   // context.params.meetupId is identified from [meetupid] folder
 
