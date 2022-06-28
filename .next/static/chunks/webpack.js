4f783bee933add67de5e0f1022219f8c5833cfeb
berkagmpp <berkagmpp@gmail.com>
2022-06-28 13:23:04 +1200

    feat: Add getServerSideProps fn in pages>index.js Add getServerSideProps fn outside of HomePage fn Add returning props object(DUMMY_DATA) in getServerSideProps fn Add context as a parameter and define req and res(node.js) -> getServerSideProps fn makes to regenerate and fetch data for every incoming requsts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "61937821e7e342b2"; }
+/******/ 		__webpack_require__.h = function() { return "826bf0f1351dd395"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
